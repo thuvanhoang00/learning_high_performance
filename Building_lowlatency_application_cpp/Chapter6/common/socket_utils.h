72b26463e6ecc7ce2a3075296405ae42fe80c146
thuvanhoang00 <thu.hoangvan00@gmail.com>
2025-07-04 22:09:10 +0700

    update socket class

--- Building_lowlatency_application_cpp/Chapter6/common/socket_utils.h
+++ Building_lowlatency_application_cpp/Chapter6/common/socket_utils.h
@@ -26,9 +26,31 @@ namespace thu{
     auto wouldBlock() -> bool;
     auto setMcastTTL(int fd, int ttl) noexcept -> bool;
     auto setTTL(int fd, int ttl) -> bool;
-    auto join(int fd, const std::string &ip, const std::string &iface, int port) -> bool;
+    auto join(int fd, const std::string &ip) -> bool;
     auto createSocket(Logger &logger, const std::string &t_ip, const std::string &iface, int port, bool is_udp, bool is_blocking,
                         bool is_listening, int ttl, bool needs_so_timestamp) -> int;
+    auto createSocket(Logger &logger, const SocketCfg &socket_cfg) -> int;
+
+    struct SocketCfg{
+        std::string ip_;
+        std::string iface_;
+        int port_ = -1;
+        bool is_udp_ = false;
+        bool is_listening_ = false;
+        bool needs_so_timestamp_ = false;
+
+        auto toString() const{
+            std::stringstream ss;
+            ss << "SocketCfg[ip:" << ip_
+            << " iface:" << iface_
+            << " port:" << port_
+            << " is_udp:" << is_udp_
+            << " is_listening:" << is_listening_
+            << " needs_SO_timestamp:" << needs_so_timestamp_
+            << "]";
+            return ss.str();
+        }
+    };
 }
 
 #endif
