514b54a283785e496afdec20ef688ec6d71bfa6a
thuvanhoang00 <thu.hoangvan00@gmail.com>
2025-07-06 10:56:11 +0700

    update build script

--- Building_lowlatency_application_cpp/Chapter7/build.sh
+++ Building_lowlatency_application_cpp/Chapter7/build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 CMAKE=$(which cmake)
-NINJA=$(which ninja)
 BUILD_DIR=cmake-build-release
+MAKE=$(which make)
 
-mkdir -p cmake-build-release
-# $CMAKE -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=$NINJA -G Ninja -S . -B cmake-build-release
-# clean
-# $CMAKE --build "$BUILD_DIR" --target clean
-# # build
-# $CMAKE --build "$BUILD_DIR" -- -j4
+mkdir -p $BUILD_DIR
+$CMAKE -DCMAKE_BUILD_TYPE=Release -S . -B $BUILD_DIR
 
-# $CMAKE --build cmake-build-release --target clean -j 4
-# $CMAKE --build cmake-build-release --target all -j 4
+$CMAKE --build $BUILD_DIR --target -j 4 clean
+$CMAKE --build $BUILD_DIR --target -j 4 all
+
+# Clean the build directory using make
+$MAKE -C $BUILD_DIR clean -j 4
+
+# Build all targets using make
+$MAKE -C $BUILD_DIR all -j 4
