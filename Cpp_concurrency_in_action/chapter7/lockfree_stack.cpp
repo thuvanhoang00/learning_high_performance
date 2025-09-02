745e9365120645803cc19e92e70e8677270279ba
thuvanhoang00 <thu.hoangvan00@gmail.com>
2025-09-02 21:11:11 +0700

    add lock_free_stack part 1

---
+++ Cpp_concurrency_in_action/chapter7/lockfree_stack.cpp
@@ -0,0 +1,56 @@
+#include <atomic>
+#include <memory>
+template<typename T>
+class lock_free_stack
+{
+private:
+    // struct node{
+    //     T data_;
+    //     node* next_;
+    //     node(T const& data) : data_(data) {}
+    // };
+    struct node{
+        std::shared_ptr<T> data_;
+        node* next_;
+        node(T const& data) : data_(std::make_shared<T>(data)) {}
+    };
+    std::atomic<node*> head_;
+
+    std::atomic<unsigned> threads_in_pop;
+    void try_reclaim(node* old_head);
+public:
+    void push(T const& data)
+    {
+        node* const new_node = new node(data);
+        new_node->next_ = head_.load();
+        while(!head_.compare_exchange_weak(new_node->next_, new_node));
+    }
+    /*Leaking code and also non-exception safety*/
+    // void pop(T& result)
+    // {
+    //     node* old_head = head_.load();
+    //     while(!head_.compare_exchange_weak(old_head, old_head->next_));
+    //     result = old_head->data_;
+    // }
+  
+    // std::shared_ptr<T> pop()
+    // {
+    //     node* old_head = head_.load(); // still have leaking
+    //     while(old_head && !head_.compare_exchange_weak(old_head, old_head->next_));
+    //     return old_head ? old_head->data_ : std::shared_ptr<T>(); /****** return shared_ptr wont throw an exeption  ******/
+    // }
+
+    std::shared_ptr<T> pop()
+    {
+        ++threads_in_pop;
+        node* old_head = head_.load();
+        while(old_head && !head_.compare_exchange_weak(old_head, old_head->next_));
+        std::shared_ptr<T> res;
+        if(old_head){
+            res.swap(old_head->data_);
+        }
+        try_reclaim(old_head);
+        return res;
+    }
+    
+};
