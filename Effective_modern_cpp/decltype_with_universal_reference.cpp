9498fe70023942bd5466939833d67ecd40cc9765
thuvanhoang00 <thu.hoangvan00@gmail.com>
2025-09-19 09:28:53 +0700

    add decltype

---
+++ Effective_modern_cpp/decltype_with_universal_reference.cpp
@@ -0,0 +1,22 @@
+#include <deque>
+#include <string>
+#include <iostream>
+template<typename Container, typename Index>
+decltype(auto) authAndAccess(Container&& c, Index i){
+    return std::forward<Container>(c)[i];
+}
+
+std::deque<std::string> makeStringDeque()
+{
+    return std::deque<std::string>{"1", "2", "3"};
+}
+
+int main(){
+    auto s = authAndAccess(makeStringDeque(), 1);
+    std::deque<std::string> queue{"a", "b", "c"};
+    auto s2 = authAndAccess(queue, 0);
+    s2 = "b";
+
+    std::cout << queue[0] << std::endl;
+    return 0;
+}
