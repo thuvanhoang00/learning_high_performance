2f0fdd283905d1ee4a0f8244621cf714045e7a71
thuvanhoang00 <thu.hoangvan00@gmail.com>
2025-10-05 11:05:56 +0700

    fix bug done

--- Practice/lockfreequeue.cpp
+++ Practice/lockfreequeue.cpp
@@ -7,7 +7,7 @@ private:
     struct Node{
         T data_;
         Node* next_;
-        Node(const T& data) : data_(data), next_(nullptr) {}
+        // Node(const T& data) : data_(data), next_(nullptr) {}
         Node() : data_{}, next_(nullptr) {}
     };
 
@@ -16,33 +16,43 @@ private:
 
 public:
     LockFreeQueue() : head_(new Node), tail_(head_.load()) {}
+    ~LockFreeQueue(){
+        while(head_){
+            auto old_head = head_.load();
+            head_.store(old_head->next_);
+            delete old_head;
+        }
+    }
 
     bool push(const T& value){
-        Node* new_node = new Node(value);
+        Node* new_node = new Node();
         auto old_tail = tail_.load();
         old_tail->next_ = new_node;
+        old_tail->data_ = value;
         tail_.store(new_node, std::memory_order_release);
         return true;
 
     }
 
     bool pop(T& value){
-        // if(head_.load() == tail_.load(std::memory_order_acquire)){
-        //     return false;
-        // }
-        // auto popped_node = head_.load();
-        // head_.store(popped_node->next_);
-        // value = popped_node->data_;
-        // return true;
+
+#ifdef NAIVE
+        if(head_.load() == tail_.load(std::memory_order_acquire)){
+            return false;
+        }
+        auto popped_node = head_.load();
+        head_.store(popped_node->next_);
+        value = popped_node->data_;
+        return true;
+#endif
+
         Node* const old_head = head_.load();
         if(old_head == tail_.load(std::memory_order_acquire)){
             return false;
         }
         head_.store(old_head->next_);
-        if(!old_head){
-            return false;
-        }
         value = old_head->data_;
+        delete old_head;
         return true;
     }
 };
