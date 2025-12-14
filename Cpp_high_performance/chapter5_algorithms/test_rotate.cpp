8db1631d95f65d49844699a0b9df3898bdd4f965
thu <thu.hoangvan00@gmail.com>
2025-12-14 20:38:18 +0700

    update

--- Cpp_high_performance/chapter5_algorithms/test_rotate.cpp
+++ Cpp_high_performance/chapter5_algorithms/test_rotate.cpp
@@ -4,7 +4,7 @@
 
 int main(){
     std::array<int, 10> arr{0,1,2,3,4,5,6,7,8,9};
-    auto new_begin = std::next(arr.begin(), 4); // to the 5th element
+    auto new_begin = std::next(arr.begin(), 4); // points to the 5th element
     // move first 4 element to the end of array->should be: 4,5,6,7,8,9,0,1,2,3
     std::rotate(arr.begin(), new_begin, arr.end());
     for(auto e : arr){
