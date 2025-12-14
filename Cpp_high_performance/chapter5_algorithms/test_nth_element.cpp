b3b869af2c8e2db29cc52ddfdd02e1969b1c7361
thu <thu.hoangvan00@gmail.com>
2025-12-14 20:21:42 +0700

    test nth_element, partial_sort

---
+++ Cpp_high_performance/chapter5_algorithms/test_nth_element.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+int main(){
+    std::vector<int> v{6,3,2,7,4,1,5};
+    auto it = v.begin() + v.size()/2;
+    auto left = it-1;
+    auto right = it+2;
+    std::nth_element(v.begin(), left, v.end());
+    std::nth_element(v.begin(), right, v.end());
+    // std::cout << *left << std::endl;
+    // std::cout << *right << std::endl;
+    std::partial_sort(left, right, v.end());
+    for(auto e: v){
+        std::cout << e << " ";
+    }
+    std::cout << "\n";
+    return 0;
+}
